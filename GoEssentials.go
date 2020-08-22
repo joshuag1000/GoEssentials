e731aea5c369c9a83fb9bb0dd793cae090d6fc26
Joshua Glass <joshuag1000@outlook.com>
2020-08-23 00:47:45 +0100

    added function that gets the devices ip

--- GoEssentials.go
+++ GoEssentials.go
@@ -3,6 +3,8 @@ package goessentials
 import (
 	"fmt"
 	"log"
+	"net"
+	"os"
 	"os/exec"
 	"runtime"
 )
@@ -26,4 +28,24 @@ func OpenBrowser(url string) {
 
 func Hello() {
 	fmt.Println("Hi")
+}
+
+func GetServerIp(ipNum int) string {
+	addrs, err := net.InterfaceAddrs()
+	if err != nil {
+		os.Stderr.WriteString("Oops: " + err.Error() + "\n")
+		os.Exit(1)
+	}
+	var ips [255]string
+	var i int
+	i = 0
+	for _, a := range addrs {
+		if ipnet, ok := a.(*net.IPNet); ok && !ipnet.IP.IsLoopback() {
+			if ipnet.IP.To4() != nil {
+				ips[i] = ipnet.IP.String()
+				i = i + 1
+			}
+		}
+	}
+	return ips[ipNum]
 }
